d93f33d9d93b7601b6ec25153b8ce94fec7ceb23
unknown <ahedodeh@outlook.com>
2022-07-30 18:10:20 +0300

    all pages

---
+++ trip/js/admin.js
@@ -0,0 +1,15 @@
+function openTable() {
+    document.getElementById('table').style.display = "block";
+}
+function closeTable() {
+    document.getElementById('table').style.display = "none";
+}
+function openPlane() {
+    document.getElementById('edit-bolgs').style.display = "block";
+}
+function closePlane() {
+    document.getElementById('edit-bolgs').style.display = "none";
+}
+function apperAdminPhoto() {
+    document.getElementById('admin-photo-card').style.display = "grid";
+}
